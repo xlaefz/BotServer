80b45a743ee6965a3b9e712a32a275f3f4034523
Jason <jasonzhe@usc.edu>
2017-02-11 19:23:36 -0800

    updated filter

--- api/moviedb.js
+++ api/moviedb.js
@@ -1,11 +1,18 @@
 module.exports = {
   search: function(searchTerm) {
     const mdb = require('moviedb')('58149d99c2f26b9bc8e4a44cd1da1300');
-    console.log("search Term" + searchTerm);
+    // console.log("search Term" + searchTerm);
     return new Promise(function(resolve, reject) {
       mdb.searchMovie({query: searchTerm}, (err,res)=>{
-        console.log(res);
-        resolve(res);
+        // console.log(res);
+        var result = res.results;
+        var formattedResults = result.map(function(result){
+          return {
+            title:result.title
+          };
+        });
+        console.log(formattedResults);
+        resolve(formattedResults);
       });
     });
   }
